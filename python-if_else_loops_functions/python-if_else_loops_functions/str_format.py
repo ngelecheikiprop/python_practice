ba6cb0ddc405192c5da173c6075ea6d574320061
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-01-21 03:54:43 +0300

    using with open source software

--- python-if_else_loops_functions/python-if_else_loops_functions/str_format.py
+++ python-if_else_loops_functions/python-if_else_loops_functions/str_format.py
@@ -3,3 +3,7 @@ print("sammy has {} ballons".format(5))
 #assign a variable to be equal to the value of a string that has formatter placeholders:
 open_string = "sammy loves {}"
 print(open_string.format("opensource"))
+#using with multiple place holders
+new_open_string =  "sammy loves {} {}"
+print(new_open_string.format("open source", "software"))
+
