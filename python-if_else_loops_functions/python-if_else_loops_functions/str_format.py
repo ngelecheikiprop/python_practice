7240682783083fd934198f55da9b6d11233b53f9
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-01-21 03:51:01 +0300

    assigning variables with format place holders

--- python-if_else_loops_functions/python-if_else_loops_functions/str_format.py
+++ python-if_else_loops_functions/python-if_else_loops_functions/str_format.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 print("sammy has {} ballons".format(5))
-
+#assign a variable to be equal to the value of a string that has formatter placeholders:
+open_string = "sammy loves {}"
+print(open_string.format("opensource"))
