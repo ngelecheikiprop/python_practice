f18300f5478822a7777fc40302a9f43a52111410
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-01-24 06:19:21 +0300

    doing more on lists

---
+++ python-data_structures_lists_Tuples/more_lists.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+fruits = ['orange', 'apple', 'pear', 'banana','kiwi', 'apple', 'banana']
+print(fruits.count('banana'))
+print(fruits.count('tangerine'))
+print(fruits.index('banana'))
+print(fruits.index('banana' , 4))
